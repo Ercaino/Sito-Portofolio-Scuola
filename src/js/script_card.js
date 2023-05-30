637cc42b54e96fb6f21d665ecca9dde437b11f2b
Fulvio <fulviosappia@gmail.com>
2023-05-30 18:37:45 +0200

    Init Page
    Inizializzazione pagine di errore, htaccess, home e stili

---
+++ src/js/script_card.js
@@ -0,0 +1,4 @@
+
+    document.getElementById("myPath1").setAttribute("d", "M 40 80 c 22 0 40 -22 40 -40 v 40 Z");
+    document.getElementById("myPath2").setAttribute("d", "M 40 80 c 22 0 40 -22 40 -40 v 40 Z");
+    document.getElementById("myPath3").setAttribute("d", "M 40 80 c 22 0 40 -22 40 -40 v 40 Z");
